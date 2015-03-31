550356958d90af4d4092226d75d8cf4b4c1d1e39
jack <jackculhane@gmail.com>
2015-03-31 20:04:51 +0100

    Removed unused imports

--- src/ie/jackculhane/haliphaxhideroot/PatchRooted.java
+++ src/ie/jackculhane/haliphaxhideroot/PatchRooted.java
@@ -2,23 +2,19 @@ package ie.jackculhane.haliphaxhideroot;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XposedBridge;
-import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class PatchRooted implements IXposedHookLoadPackage {
 	
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
 	{
-		// com/cryptomathic/securecore/TrusteerMalwareDetector
-		
+		// com/cryptomathic/securecore/TrusteerMalwareDetector	
 	    if (
-	    		lpparam.packageName.equals("com.cryptomathic.securecore") ||
-	    		lpparam.packageName.equals("com.grppl.android.shell.halifax") ||
-	    		lpparam.packageName.equals("com.grppl.android.shell.CMBlloydsTSB73") ||
+	    	lpparam.packageName.equals("com.cryptomathic.securecore") ||
+	    	lpparam.packageName.equals("com.grppl.android.shell.halifax") ||
+	    	lpparam.packageName.equals("com.grppl.android.shell.CMBlloydsTSB73") ||
 			lpparam.packageName.equals("com.grppl.android.shell.BOS")
 	    )
 	    {
