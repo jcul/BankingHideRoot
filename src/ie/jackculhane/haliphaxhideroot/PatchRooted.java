8a3ee548200d6d902a0b3fcfb1fe5d6afb0e247a
jack <jackculhane@gmail.com>
2015-06-01 15:04:34 +0100

    Tabs to spaces

--- src/ie/jackculhane/haliphaxhideroot/PatchRooted.java
+++ src/ie/jackculhane/haliphaxhideroot/PatchRooted.java
@@ -7,29 +7,29 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XposedBridge;
 
 public class PatchRooted implements IXposedHookLoadPackage {
-	
-	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
-	{
-	    if (
-	    	lpparam.packageName.equals("com.cryptomathic.securecore") ||
-	    	lpparam.packageName.equals("com.grppl.android.shell.halifax") ||
-	    	lpparam.packageName.equals("com.grppl.android.shell.CMBlloydsTSB73") ||
-	    	lpparam.packageName.equals("com.grppl.android.shell.BOS") ||
-	    	lpparam.packageName.equals("uk.co.tsb.mobilebank")
-	    )
-	    {	
-	    	XposedBridge.log("Loaded " + lpparam.packageName + ", hooking root methods");
-	    	
-	    	try
-	    	{
-		    	findAndHookMethod("com.cryptomathic.securecore.MalwareDetection", lpparam.classLoader, "getIsPossiblyRooted", XC_MethodReplacement.returnConstant(false));
-		    	findAndHookMethod("com.cryptomathic.securecore.MalwareDetection", lpparam.classLoader, "getIsRooted", XC_MethodReplacement.returnConstant(false));
-	    	}
-	    	catch (NoSuchMethodError e)
-	    	{
-	    		XposedBridge.log("Unable to hook new methods, will attempt to patch old isRooted method");
-	    		findAndHookMethod("com.cryptomathic.securecore.TrusteerMalwareDetector", lpparam.classLoader, "isRooted", XC_MethodReplacement.returnConstant(false));
-	    	}
-	    }	    
-	}
+    
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
+    {
+        if (
+            lpparam.packageName.equals("com.cryptomathic.securecore") ||
+            lpparam.packageName.equals("com.grppl.android.shell.halifax") ||
+            lpparam.packageName.equals("com.grppl.android.shell.CMBlloydsTSB73") ||
+            lpparam.packageName.equals("com.grppl.android.shell.BOS") ||
+            lpparam.packageName.equals("uk.co.tsb.mobilebank")
+        )
+        {    
+            XposedBridge.log("Loaded " + lpparam.packageName + ", hooking root methods");
+            
+            try
+            {
+                findAndHookMethod("com.cryptomathic.securecore.MalwareDetection", lpparam.classLoader, "getIsPossiblyRooted", XC_MethodReplacement.returnConstant(false));
+                findAndHookMethod("com.cryptomathic.securecore.MalwareDetection", lpparam.classLoader, "getIsRooted", XC_MethodReplacement.returnConstant(false));
+            }
+            catch (NoSuchMethodError e)
+            {
+                XposedBridge.log("Unable to hook new methods, will attempt to patch old isRooted method");
+                findAndHookMethod("com.cryptomathic.securecore.TrusteerMalwareDetector", lpparam.classLoader, "isRooted", XC_MethodReplacement.returnConstant(false));
+            }
+        }        
+    }
 }
