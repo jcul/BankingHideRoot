8d778aabf101c958beeef557b9f14043646f386f
jack <jackculhane@gmail.com>
2015-06-01 15:03:59 +0100

    Added support for newer versions of apps

--- src/ie/jackculhane/haliphaxhideroot/PatchRooted.java
+++ src/ie/jackculhane/haliphaxhideroot/PatchRooted.java
@@ -10,17 +10,26 @@ public class PatchRooted implements IXposedHookLoadPackage {
 	
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
 	{
-		// com/cryptomathic/securecore/TrusteerMalwareDetector	
 	    if (
 	    	lpparam.packageName.equals("com.cryptomathic.securecore") ||
 	    	lpparam.packageName.equals("com.grppl.android.shell.halifax") ||
 	    	lpparam.packageName.equals("com.grppl.android.shell.CMBlloydsTSB73") ||
-			lpparam.packageName.equals("com.grppl.android.shell.BOS") ||
-			lpparam.packageName.equals("uk.co.tsb.mobilebank")
+	    	lpparam.packageName.equals("com.grppl.android.shell.BOS") ||
+	    	lpparam.packageName.equals("uk.co.tsb.mobilebank")
 	    )
-	    {
-	    	XposedBridge.log("Loaded " + lpparam.packageName + " replacing isRooted() method");
-	    	findAndHookMethod("com.cryptomathic.securecore.TrusteerMalwareDetector", lpparam.classLoader, "isRooted", XC_MethodReplacement.returnConstant(false));
-	    }
+	    {	
+	    	XposedBridge.log("Loaded " + lpparam.packageName + ", hooking root methods");
+	    	
+	    	try
+	    	{
+		    	findAndHookMethod("com.cryptomathic.securecore.MalwareDetection", lpparam.classLoader, "getIsPossiblyRooted", XC_MethodReplacement.returnConstant(false));
+		    	findAndHookMethod("com.cryptomathic.securecore.MalwareDetection", lpparam.classLoader, "getIsRooted", XC_MethodReplacement.returnConstant(false));
+	    	}
+	    	catch (NoSuchMethodError e)
+	    	{
+	    		XposedBridge.log("Unable to hook new methods, will attempt to patch old isRooted method");
+	    		findAndHookMethod("com.cryptomathic.securecore.TrusteerMalwareDetector", lpparam.classLoader, "isRooted", XC_MethodReplacement.returnConstant(false));
+	    	}
+	    }	    
 	}
 }
